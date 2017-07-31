f61d6ebbdd3073cc49a85373dc1022a36c6d3d3f
Nikita <nd@crtweb.com>
2017-07-31 16:33:32 +0500

    split config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,42 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
 
 const PATHS = {
   source: path.join(__dirname, 'source'),
   build: path.join(__dirname, 'build')
 };
-module.exports = {
-  entry: {
-    'index': PATHS.source + '/pug/index.js'
-  },
-  output: {
-      path: PATHS.build,
-      filename: '[name].js'
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      filename: 'index.html',
-      chunks: ['index'],
-      template: PATHS.source + '/pug/index.pug'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: true
-        }
-      }
+
+const common = merge([
+  {
+    entry: {
+      'index': PATHS.source + '/pug/index.js'
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        filename: 'index.html',
+        chunks: ['index'],
+        template: PATHS.source + '/pug/index.pug'
+      })
     ]
+  },
+  pug()
+]);
+
+module.exports = function(env) {
+  if (env === 'production') {
+    return common;
+  }
+  if (env === 'development') {
+    return merge([
+        common,
+        devserver()
+    ])
   }
 };
